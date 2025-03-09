9fd3e899e548b3e5b1cac14a30fcad3fb7553a0d
Alex-Alen <alex-alen.pugatsov@tptlive.ee>
2025-03-09 07:20:34 +0200

    update

--- lib/screens/author.dart
+++ lib/screens/author.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackernews/widgets/hacker_news_notifier.dart';
 import 'package:intl/intl.dart';
 import 'package:dio/dio.dart';
 
@@ -33,105 +34,88 @@ class Author extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Author's Page")),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.grey[200],
-              ),
-              height: 45,
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Icon(Icons.person, color: Colors.black54),
-                  ),
-                  Text(
-                    capitalizedAuthorName,
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.black54,
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: fetchUserData(authorName),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error fetching user data'));
+          } else if (snapshot.hasData) {
+            final userData = snapshot.data!;
+            final creationDate = userData['created'];
+            final submittedIds = userData['submitted'] as List<dynamic>? ?? [];
+            return Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[200],
                     ),
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 7),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(9),
-                        color: Colors.white,
-                      ),
-                      height: 28,
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: FutureBuilder<Map<String, dynamic>>(
-                              future: fetchUserData(authorName),
-                              builder: (context, snapshot) {
-                                if (snapshot.connectionState ==
-                                    ConnectionState.waiting) {
-                                  return Text(
-                                    'Loading...',
+                    height: 45,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Icon(Icons.person, color: Colors.black54),
+                        ),
+                        Text(
+                          capitalizedAuthorName,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(9),
+                              color: Colors.white,
+                            ),
+                            height: 28,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: Text(
+                                    formatCreationDate(creationDate),
                                     style: TextStyle(
                                       fontSize: 10,
                                       fontWeight: FontWeight.w800,
                                       color: Colors.black87,
                                     ),
-                                  );
-                                } else if (snapshot.hasError) {
-                                  return Text(
-                                    'Error fetching data',
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w800,
-                                      color: Colors.black87,
-                                    ),
-                                  );
-                                } else if (snapshot.hasData) {
-                                  final userData = snapshot.data!;
-                                  final creationDate = userData['created'];
-                                  return Text(
-                                    'Author since ${formatCreationDate(creationDate)}',
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w800,
-                                      color: Colors.black87,
-                                    ),
-                                  );
-                                } else {
-                                  return Text(
-                                    'No data available',
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w800,
-                                      color: Colors.black87,
-                                    ),
-                                  );
-                                }
-                              },
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          // Expanded(
-          //   child: HackerNewsList(
-          //     endpoint:
-          //         'https://hacker-news.firebaseio.com/v0/user/$authorName.json',
-          //     showProfileButton: false,
-          //   ),
-          // ),
-        ],
+                ),
+                Expanded(
+                  child: HackerNewsList(
+                    showProfileButton: false,
+                    ids: submittedIds.cast<int>(),
+                    resetIds: () {
+                      ref
+                          .read(hackerNewsProvider.notifier)
+                          .reset(submittedIds.cast<int>());
+                    },
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Center(child: Text('No data available'));
+          }
+        },
       ),
     );
   }
--- lib/widgets/hacker_news_notifier.dart
+++ lib/widgets/hacker_news_notifier.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
-import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:toastification/toastification.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+// Dio provider for network requests
 final dioProvider = Provider((ref) => Dio());
 
-final hackerNewsProvider =
-    StateNotifierProvider<HackerNewsNotifier, List<Map<String, dynamic>>>((
-      ref,
-    ) {
-      return HackerNewsNotifier(ref, []);
-    });
+// StateNotifierProvider for managing Hacker News data
+final hackerNewsProvider = StateNotifierProvider<HackerNewsNotifier, List<Map<String, dynamic>>>((ref) {
+  return HackerNewsNotifier(ref, []);
+});
 
 class HackerNewsNotifier extends StateNotifier<List<Map<String, dynamic>>> {
   HackerNewsNotifier(this.ref, List<int> ids) : super([]) {
@@ -27,20 +25,26 @@ class HackerNewsNotifier extends StateNotifier<List<Map<String, dynamic>>> {
   List<int> _allIds = [];
   bool _isLoading = false;
 
+  // Reset the state with new IDs
+  void reset(List<int> newIds) {
+    _page = 0;
+    _allIds = newIds;
+    state = [];
+    fetchNextBatch();
+  }
+
+  // Fetch the next batch of posts
   Future<void> fetchNextBatch() async {
     if (_isLoading || _page * _pageSize >= _allIds.length) return;
     _isLoading = true;
 
     final ids = _allIds.skip(_page * _pageSize).take(_pageSize).toList();
-    final futures = ids.map(
-      (id) async => await ref
-          .read(dioProvider)
-          .get("https://hacker-news.firebaseio.com/v0/item/$id.json"),
-    );
-    final responses = await Future.wait(futures);
+    final futures = ids.map((id) async {
+      final response = await ref.read(dioProvider).get("https://hacker-news.firebaseio.com/v0/item/$id.json");
+      return response.data as Map<String, dynamic>;
+    });
 
-    final newData =
-        responses.map((r) => r.data as Map<String, dynamic>).toList();
+    final newData = await Future.wait(futures);
     state = [...state, ...newData];
     _page++;
     _isLoading = false;
@@ -49,12 +53,10 @@ class HackerNewsNotifier extends StateNotifier<List<Map<String, dynamic>>> {
 
 class HackerNewsList extends ConsumerStatefulWidget {
   final List<int> ids;
-  final bool showProfileButton;
 
   const HackerNewsList({
     super.key,
     required this.ids,
-    this.showProfileButton = true,
   });
 
   @override
@@ -67,96 +69,75 @@ class _HackerNewsListState extends ConsumerState<HackerNewsList> {
   @override
   void initState() {
     super.initState();
-    ref.read(hackerNewsProvider.notifier)._allIds = widget.ids;
-    ref.read(hackerNewsProvider.notifier).fetchNextBatch();
+    ref.read(hackerNewsProvider.notifier).reset(widget.ids);
     _scrollController.addListener(_onScroll);
   }
 
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  // Handle scroll events to load more data
   void _onScroll() {
-    if (_scrollController.position.pixels >=
-        _scrollController.position.maxScrollExtent - 100) {
+    if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 100) {
       ref.read(hackerNewsProvider.notifier).fetchNextBatch();
     }
   }
 
+  // Open a URL in the browser
   void _openUrl(String url) async {
     if (!await launchUrl(Uri.parse(url))) {
-      throw Exception('Could not launch $url');
+      toastification.show(
+        title: const Text("Could not launch the URL"),
+        type: ToastificationType.error,
+        autoCloseDuration: const Duration(seconds: 3),
+      );
     }
-  }
-
-  void _onProfileTap(String author) {
-    context.push('/author/$author');
   }
 
   @override
   Widget build(BuildContext context) {
     final posts = ref.watch(hackerNewsProvider);
-    final isLoading = ref.watch(hackerNewsProvider.notifier)._isLoading;
+    final isLoading = ref.read(hackerNewsProvider.notifier)._isLoading;
 
     return Stack(
       children: [
-        if (posts.isEmpty)
-          FutureBuilder(
-            future: Future.delayed(const Duration(seconds: 3)),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (posts.isEmpty) {
-                return const Center(child: Text("There is no available posts"));
-              }
-              return const SizedBox();
-            },
-          )
-        else
-          ListView.builder(
-            controller: _scrollController,
-            itemCount: posts.length,
-            itemBuilder: (context, index) {
-              final post = posts[index];
-              final time = DateTime.fromMillisecondsSinceEpoch(
-                post['time'] * 1000,
-              );
-              final formattedTime = DateFormat('yyyy-MM-dd HH:mm').format(time);
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 5,
+        ListView.builder(
+          controller: _scrollController,
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            final post = posts[index];
+            final time = DateTime.fromMillisecondsSinceEpoch(post['time'] * 1000);
+            final formattedTime = DateFormat('yyyy-MM-dd HH:mm').format(time);
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: ListTile(
-                    leading:
-                        widget.showProfileButton
-                            ? GestureDetector(
-                              onTap: () => _onProfileTap(post['by']),
-                              child: const Icon(
-                                Icons.person,
-                                color: Colors.black54,
-                              ),
-                            )
-                            : null,
-                    title: Text(post['title'] ?? 'No Title'),
-                    subtitle: Text('By ${post['by']} · $formattedTime'),
-                    onTap: () {
-                      if (post['url'] != null) {
-                        _openUrl(post['url']);
-                      } else {
-                        toastification.show(
-                          title: const Text("No link available for this item"),
-                          type: ToastificationType.error,
-                          autoCloseDuration: const Duration(seconds: 3),
-                        );
-                      }
-                    },
-                  ),
+                child: ListTile(
+                  title: Text(post['title'] ?? 'No Title'),
+                  subtitle: Text('By ${post['by']} · $formattedTime'),
+                  onTap: () {
+                    if (post['url'] != null) {
+                      _openUrl(post['url']);
+                    } else {
+                      toastification.show(
+                        title: const Text("No link available for this item"),
+                        type: ToastificationType.error,
+                        autoCloseDuration: const Duration(seconds: 3),
+                      );
+                    }
+                  },
                 ),
-              );
-            },
-          ),
+              ),
+            );
+          },
+        ),
         if (isLoading && posts.isNotEmpty)
           Positioned(
             bottom: 30,
